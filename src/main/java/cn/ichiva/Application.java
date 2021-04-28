3498f4f80dae77fde9c0f1c82669171da35b3a53
Chiva <874460517@qq.com>
2021-04-28 18:08:19 +0800

    不负亲门

--- src/main/java/cn/ichiva/Application.java
+++ src/main/java/cn/ichiva/Application.java
@@ -7,10 +7,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
+@CrossOrigin
 @RestController
 @Configuration
 @SpringBootApplication
@@ -22,7 +30,14 @@ public class Application{
 
 	@GetMapping("/version")
 	public Object version(){
-		return new Object(){ public String version = "v0.0.1"; };
+		return new Object(){ public String version = "v0.0.2"; };
+	}
+
+
+	@RequestMapping("/load")
+	public Object load() throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get("./def.json"));
+		return new String(bytes);
 	}
 
 	@Bean
